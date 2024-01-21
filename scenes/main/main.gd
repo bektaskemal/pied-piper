eb3bd34da651235c405bd2ed8d51a1d84b96a582
Kemal Bektas <kemalbektas16@gmail.com>
2024-01-21 15:20:54 +0100

    feat: update joystick sprite and add pause button

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -10,6 +10,7 @@ var pause_menu_scene = preload("res://scenes/UI/pause_menu.tscn")
 func _ready():
 	randomize()
 	player.health_component.died.connect(on_died)
+	$TouchControls/Button.pressed.connect(on_pause_pressed)
 
 func on_died():
 	var defeat_screen = end_screen_scene.instantiate()
@@ -20,7 +21,10 @@ func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		add_child(pause_menu_scene.instantiate())
 		get_tree().root.set_input_as_handled()
-	
+
+func on_pause_pressed():
+	add_child(pause_menu_scene.instantiate())
+
 func _notification(what):
 	if what == MainLoop.NOTIFICATION_APPLICATION_FOCUS_OUT || what == NOTIFICATION_WM_WINDOW_FOCUS_OUT:
 		if not get_tree().paused:
